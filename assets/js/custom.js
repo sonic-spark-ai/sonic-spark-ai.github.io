246d38b7658f34def54a94d4f6139a8bb4aa60f2
627md-Tajul-Islam <tajul.azazil@gmail.com>
2024-10-05 20:20:50 +0600

    okh

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,26 @@
+// explore button animation
 document.getElementById('scrollBtn').addEventListener('click', function() {
     // Smooth scroll to the target element
     document.getElementById('wowWork').scrollIntoView({ behavior: 'smooth' });
   });
 
 
+// back to top button
+  var btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+  
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+  
+  if ($(window).scrollTop() > 100) {
+    btn.addClass('show');
+  }
