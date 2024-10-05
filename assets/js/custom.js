d83d3b14368e44a73fb6a8e58292fd3a0c840245
627md-Tajul-Islam <tajul.azazil@gmail.com>
2024-10-05 15:39:01 +0600

    ok

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,3 +2,5 @@ document.getElementById('scrollBtn').addEventListener('click', function() {
     // Smooth scroll to the target element
     document.getElementById('wowWork').scrollIntoView({ behavior: 'smooth' });
   });
+
+
